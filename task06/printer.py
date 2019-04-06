8c5d04e6ec651581cf1d392f8ccee2d93a5f3f6f
Andrew Kislitsyn <kisandy@yandex.ru>
2019-04-06 17:55:54 +0300

    Compelte task 2

---
+++ task06/printer.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+from model import *
+
+DEFAULT_INDENT = 4
+
+
+class PrettyPrint(ASTNodeVisitor):
+    def __init__(self):
+        self.indent_size = 0
+
+    def get_indent(self):
+        res = ''
+        return res + self.indent_size * ' '
+
+    def make_end_of_statement(self, res):
+        if not res.endswith('}'):
+            res += ';'
+        res += '\n'
+        return res
+
+    def check_in_brackets(self, expr):
+        if not (expr.startswith('(')
+                and expr.endswith(')')):
+            expr = '(' + expr + ')'
+        return expr
+
+    def visit_number(self, number_obj):
+        return str(number_obj.value)
+
+    def visit_function(self, function_obj):
+        pass
+
+    def visit_function_definition(self, function_definition_obj):
+        res = 'def ' + function_definition_obj.name
+        self.indent_size += DEFAULT_INDENT
+        cur_func = function_definition_obj.function
+        res += '('
+        if cur_func.args:
+            res += cur_func.args[0]
+            for arg in cur_func.args[1:]:
+                res += ', ' + arg
+        res += ')'
+        res += ' {\n'
+        for statement in cur_func.body:
+            res += self.get_indent() + statement.accept(self)
+            res = self.make_end_of_statement(res)
+        self.indent_size -= DEFAULT_INDENT
+        res += self.get_indent() + '}'
+        return res
+
+    def visit_conditional(self, conditional_obj):
+        condition = self.check_in_brackets(
+            conditional_obj.condition.accept(self))
+        res = 'if ' + condition + ' {\n'
+        self.indent_size += DEFAULT_INDENT
+        for statement in conditional_obj.if_true or []:
+            res += self.get_indent() + statement.accept(self)
+            res = self.make_end_of_statement(res)
+        self.indent_size -= DEFAULT_INDENT
+        if conditional_obj.if_false:
+            res += self.get_indent() + '} else {\n'
+            self.indent_size += DEFAULT_INDENT
+            for statement in conditional_obj.if_false or []:
+                res += self.get_indent() + statement.accept(self)
+                res = self.make_end_of_statement(res)
+            self.indent_size -= DEFAULT_INDENT
+        res += self.get_indent() + '}'
+        return res
+
+    def visit_print(self, print_obj):
+        return 'print ' + print_obj.expr.accept(self)
+
+    def visit_read(self, read_obj):
+        return 'read ' + read_obj.name
+
+    def visit_function_call(self, function_call_obj):
+        res = function_call_obj.fun_expr.accept(self)
+        res += '('
+        if function_call_obj.args:
+            res += function_call_obj.args[0].accept(self)
+            for arg in function_call_obj.args[1:]:
+                res += ', ' + arg.accept(self)
+        res += ')'
+        return res
+
+    def visit_reference(self, reference_obj):
+        return reference_obj.name
+
+    def visit_binary_operation(self, binary_operation_obj):
+        lhs = self.check_in_brackets(binary_operation_obj.lhs.accept(self))
+        rhs = self.check_in_brackets(binary_operation_obj.rhs.accept(self))
+        return '(' + lhs + ' ' + binary_operation_obj.op + ' ' + rhs + ')'
+
+    def visit_unary_operation(self, unary_operation_obj):
+        expr = self.check_in_brackets(unary_operation_obj.expr.accept(self))
+        return '(' + unary_operation_obj.op + expr + ')'
+
+    def visit_program(self, program):
+        res = program.accept(self)
+        if not res.endswith('}'):
+            res += ';'
+        return res
+
+
+def pretty_print(program):
+    printer = PrettyPrint()
+    print(printer.visit_program(program))
---
+++ task06/test_printer.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import pytest
+from model import *
+from printer import *
+
+
+def test_vist_conditional():
+    printer = PrettyPrint()
+    res = printer.visit_program(Conditional(Number(42), [], []))
+    expected_result = 'if (42) {\n}'
+    assert res == expected_result
+
+
+def test_vist_function_definition():
+    printer = PrettyPrint()
+    res = printer.visit_program(FunctionDefinition("foo", Function([], [])))
+    expected_result = 'def foo() {\n}'
+    assert res == expected_result
+
+
+def test_vist_print():
+    printer = PrettyPrint()
+    res = printer.visit_program(Print(Number(42)))
+    expected_result = 'print 42;'
+    assert res == expected_result
+
+
+def test_vist_read():
+    printer = PrettyPrint()
+    res = printer.visit_program(Read('x'))
+    expected_result = 'read x;'
+    assert res == expected_result
+
+
+def test_vist_number():
+    printer = PrettyPrint()
+    res = printer.visit_program(Number(10))
+    expected_result = '10;'
+    assert res == expected_result
+
+
+def test_vist_reference():
+    printer = PrettyPrint()
+    res = printer.visit_program(Reference('x'))
+    expected_result = 'x;'
+    assert res == expected_result
+
+
+def test_vist_binary_operation():
+    printer = PrettyPrint()
+    add = BinaryOperation(Number(2), '+', Number(3))
+    mul = BinaryOperation(Number(1), '*', add)
+    res = printer.visit_program(mul)
+    expected_result = '((1) * ((2) + (3)));'
+    assert res == expected_result
+
+
+def test_vist_unary_operation():
+    printer = PrettyPrint()
+    res = printer.visit_program(UnaryOperation('-', Number(42)))
+    expected_result = '(-(42));'
+    assert res == expected_result
+
+
+def test_pretty_print(capsys):
+    pretty_print(FunctionDefinition('main', Function(['arg1'], [
+        Read('x'),
+        Print(Reference('x')),
+        Conditional(
+            BinaryOperation(Number(2), '==', Number(3)),
+            [
+                Conditional(Number(1), [], [])
+            ],
+            [
+                FunctionCall(Reference('exit'), [
+                    UnaryOperation('-', Reference('arg1'))
+                ])
+            ],
+        ),
+    ])))
+    expected_result = \
+        'def main(arg1) {\n' + \
+        '    read x;\n' + \
+        '    print x;\n' + \
+        '    if ((2) == (3)) {\n' + \
+        '        if (1) {\n' + \
+        '        }\n' + \
+        '    } else {\n' + \
+        '        exit((-(arg1)));\n' + \
+        '    }\n' + \
+        '}\n'
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == expected_result
+
+
+if __name__ == "__main__":
+    pytest.main()
