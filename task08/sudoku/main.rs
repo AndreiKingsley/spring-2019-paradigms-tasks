9eba54d74428357fe408b9589cffa0868a88ca5b
Andrew Kislitsyn <kisandy@yandex.ru>
2019-04-23 22:15:51 +0300

    Fix unwrap to _or

--- task08/sudoku/main.rs
+++ task08/sudoku/main.rs
@@ -173,7 +173,7 @@ fn find_solution(f: &mut Field) -> Option<Field> {
 fn find_solution_parallel(mut f: Field) -> Option<Field> {
     let pool = threadpool::ThreadPool::new(8);
     let (tx, rx) = std::sync::mpsc::channel();
-    pool.execute(move || tx.send(find_solution(&mut f)).unwrap());
+    pool.execute(move || tx.send(find_solution(&mut f)).unwrap_or(()));
     rx.into_iter().find_map(|x| x)
 }
 
